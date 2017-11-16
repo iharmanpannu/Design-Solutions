df61c55022742a39497170f84de7498769cdc289
Harman Pannu <ukpannu@gmail.com>
2017-11-16 15:25:07 -0800

    Final Project

--- js/main-min.js
+++ js/main-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".burger-nav").on("click",function(){$(".nav_links ul").toggleClass("open")})});
+$(document).ready(function(){$(".burger-nav").on("click",function(){$(".nav_links ul").toggleClass("open")})}),$(document).ready(function(){$(window).on("scroll",function(){$(window).scrollTop()>120?($(".nav_links").css("background","rgba(46,60,64,.5)"),$(".nav_links a").css("color","#fff")):($(".nav_links").css("background","rgba(255,255,255,0)"),$(".nav_links a").css("color","#2e3c40"))})});
--- js/main.js
+++ js/main.js
@@ -12,3 +12,17 @@ $(document).ready(function() {
 // {
 //    $('body').removeClass("fixedPosition");
 // }
+
+$(document).ready(function(){
+	$(window).on("scroll",function(){
+  	var wn = $(window).scrollTop();
+    if(wn > 120){
+    	$(".nav_links").css("background","rgba(46,60,64,.5)");
+      $(".nav_links a").css("color","#fff");
+    }
+    else{
+    	$(".nav_links").css("background","rgba(255,255,255,0)");
+      $(".nav_links a").css("color","#2e3c40");
+    }
+  });
+});
