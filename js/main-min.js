a15274f3561ae59c92cbfe98bde844b645da684d
Harman Pannu <ukpannu@gmail.com>
2017-11-16 10:20:07 -0800

    Updated Website

---
+++ js/main-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".burger-nav").on("click",function(){$(".nav_links ul").toggleClass("open")})});
---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $(".burger-nav").on("click", function() {
+    $(".nav_links ul").toggleClass("open");
+  });
+});
