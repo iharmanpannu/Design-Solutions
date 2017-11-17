1dabd7bbedbeb5853386cbe1ded12b3d8332e129
Harman Pannu <ukpannu@gmail.com>
2017-11-17 11:48:38 -0800

    CSS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-// const navToggle = document.querySelector('.burger-nav')
-
-// navToggle.addEventListener('click', _ => {
-//   console.log('test')
-// })
 $(document).ready(function() {
   $(".burger-nav").on("click", function() {
     $(".nav_links ul").toggleClass("open");
@@ -11,23 +6,23 @@ $(document).ready(function() {
 
 if ($('#logo``').is(':visible'))
 {
-   $('body').addClass("fixedPosition");
+   $('body').addClass("fixed-position");
 }
 else
 {
-   $('body').removeClass("fixedPosition");
+   $('body').removeClass("fixed-position");
 }
 
-// $(document).ready(function(){
-// 	$(window).on("scroll",function(){
-//   	var wn = $(window).scrollTop();
-//     if(wn > 120){
-//     	$(".nav_links").css("background","rgba(46,60,64,.5)");
-//       $(".nav_links a").css("color","#fff");
-//     }
-//     else{
-//     	$(".nav_links").css("background","rgba(255,255,255,0)");
-//       $(".nav_links a").css("color","#2e3c40");
-//     }
-//   });
-// });
+$(document).ready(function(){
+	$(window).on("scroll",function(){
+  	var wn = $(window).scrollTop();
+    if(wn > 120){
+    	$(".nav_links").css("background","rgba(46,60,64,.5)");
+      $(".nav_links a").css("color","#fff");
+    }
+    else{
+    	$(".nav_links").css("background","rgba(255,255,255,0)");
+      $(".nav_links a").css("color","#2e3c40");
+    }
+  });
+});
