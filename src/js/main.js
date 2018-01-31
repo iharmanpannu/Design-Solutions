9ef83ddc5070f872496e1558f9d91e7565d78fa0
iharmanpannu <iharmanpannu@gmail.com>
2018-01-31 01:58:10 -0800

    finished projecct

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 const $mobileNav = $(".mobile-nav");
 $mobileNav.hide();
 $("#open-tog").on("click", () => {
-  $mobileNav.slideDown().show();
+  $mobileNav.animate({ width: "toggle" }, 350).show(500);
 });
 $("#close-tog").on("click", () => {
-  $mobileNav.slideToggle().hide(1000);
+  $mobileNav.animate({ width: "toggle" }, 300).hide(100);
 });
