00656bfd3c5ddb23523ff9bd2a93ff99456673fa
iharmanpannu <iharmanpannu@gmail.com>
2018-01-31 02:29:35 -0800

    complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-//To open nav
+//To open navigation
 const $mobileNav = $(".mobile-nav");
 $mobileNav.hide();
 $("#open-tog").on("click", () => {
   $mobileNav.animate({ width: "toggle" }, 350).show(500);
 });
-//To close nav
+//To close navigation
 $("#close-tog").on("click", () => {
   $mobileNav.animate({ width: "toggle" }, 300).hide(100);
 });
